e2b3ee4068cf6d647734d5e160c96e7a323d8e0f
Administrator <denghang1989@163.com>
2018-06-29 15:39:01 +0800

    提交代码

---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>用户登入</span>
+      </div>
+      <el-form ref="form" :model="form" label-width="80px" label-position="top">
+        <el-form-item label="用户名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        
+        <el-form-item label="用户密码">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        
+        <el-button class="login_button" type="primary">登入</el-button>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        form: {
+          name: "",
+          password: ""
+        }
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .login {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: column;
+  }
+  
+  .login .box-card {
+    margin: 150px auto;
+    width: 450px;
+    height: 380px;
+  }
+  
+  .login_button {
+    width: 100%;
+    margin-top: 20px;
+  }
+
+</style>
